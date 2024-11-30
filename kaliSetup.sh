285de24cdffcfc1cc03a0019e72e7b2a6ede1d65
anes@exemple.com <anes@exemple.com>
2024-11-30 11:34:17 +0100

    changed

--- kaliSetup.sh
+++ kaliSetup.sh
@@ -7,7 +7,8 @@ sudo chmod +x /usr/local/bin/pyget
 
 # repo installs
 sudo apt update -y
-sudo apt install -y go-lang pipx 
+sudo apt install -y curl wget vim git whois netcat-traditional
+sudo apt install -y python3-pip golang pipx python-is-python3
 sudo apt install -y spiderfoot 
 sudo apt install -y sherlock 
 sudo apt install -y dirsearch 
@@ -34,9 +35,7 @@ sudo apt install -y gowitness
 sudo apt install -y aquatone
 sudo apt install -y waybackurls
 sudo apt install -y dirsearch
-sudo apt install -y gau
 sudo apt install -y gospider
-sudo apt install -y knock
 sudo apt install -y fierce
 sudo apt install -y dnsgen
 sudo apt install -y https
@@ -56,6 +55,11 @@ sudo apt install -y httprobe
 # 
 pipx install bbot
 pipx install arjun
+pipx install uro
+pipx install json2paths
+pipx install parth
+pipx install proxify
+
 
 pyget https://github.com/xnl-h4ck3r/xnLinkFinder
 pyget https://github.com/m4ll0k/SecretFinder
@@ -66,6 +70,15 @@ pyget https://github.com/xnl-h4ck3r/xnLinkFinder.git
 pyget https://github.com/WangYihang/GitHacker
 pyget https://github.com/m4ll0k/SecretFinder
 pyget https://github.com/securing/DumpsterDiver
+https://github.com/guelfoweb/knock
+https://github.com/s0md3v/XSStrike
+https://github.com/s0md3v/Photon
+https://github.com/s0md3v/Bolt
+https://github.com/s0md3v/Breacher
+https://github.com/s0md3v/Decodify
+https://github.com/s0md3v/Striker
+https://github.com/s0md3v/Silver
+https://github.com/s0md3v/Zen
 
 # go installations
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
@@ -84,6 +97,8 @@ go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
 go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
 go install -v github.com/projectdiscovery/proxify/cmd/proxify@latest
 go install github.com/projectdiscovery/alterx/cmd/alterx@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/unfurl@latest
 
 GO111MODULE=on go install -v github.com/lc/subjs@latest
 go install -v github.com/musana/fuzzuli@latest
